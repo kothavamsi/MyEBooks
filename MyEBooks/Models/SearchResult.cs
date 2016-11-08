2656cf56cf1fcdd9a393c4a64c3139ada83cf884
kothavamsi <kothavamsikrishna@gmail.com>
2016-11-08 17:24:32 +0530

    intermediate..

---
+++ MyEBooks/Models/SearchResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEBooks.Models
+{
+    public class SearchResult
+    {
+        public string SearchKeyword { get; set; }
+        public IList<Book> Books { get; set; }
+    }
+}
