e524a0fcee7098b6ca9ff1d8c3d4f77f2ec613ae
kothavamsi <kothavamsikrishna@gmail.com>
2016-11-26 00:29:04 +0530

    footer part partial done

--- MyEBooks/Models/ContactUs.cs
+++ MyEBooks/Models/ContactUs.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyEBooks.Models
 {
     public class ContactUs
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter First name")]
         public string Fname { get; set; }
+        [Required(ErrorMessage = "Please enter Last name")]
         public string Lname { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         public string Description { get; set; }
         public DateTime CreatedOn { get; set; }
